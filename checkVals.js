1b98a39b442c3761435e876dbf1853ce59da850a
Joel <vashappnin107@gmail.com>
2024-04-24 16:46:27 +0530

    first commit

--- checkVals.js
+++ checkVals.js
@@ -1,26 +1,43 @@
-const betNumberInput = document.getElementById('bet-number');
-const betAmountInput = document.getElementById('bet-amount');
-const spinRoult = document.getElementById('spin-button'); 
-const result = document.getElementById('result');
-const betTypeSelect = document.getElementById('bet-type');
+const betNumberInput = document.getElementById("bet-number");
+const betAmountInput = document.getElementById("bet-amount");
+const spinRoult = document.getElementById("spin-button");
+const result = document.getElementById("result");
+const betTypeSelect = document.getElementById("bet-type");
+const numdis = document.querySelector("#number");
+const evedis = document.querySelector("#even");
+const odddids = document.querySelector("#odd");
+const reddis = document.querySelector("#red");
+const blackdis = document.querySelector("#black");
+const rowdis = document.querySelector("#row");
+const columndis = document.querySelector("#column");
 
-const bet = betTypeSelect.value;
-if(bet === "odd" || bet === "even" || bet === "red" || bet === "black") {
-    document.getElementById('bet-number').disabled = true;
+const bet1 = betTypeSelect.value;
+if (bet1 === "odd" || bet1 === "even" || bet1 === "red" || bet1 === "black") {
+  document.getElementById("bet-number").disabled = true;
 }
 
 const og = () => {
-    document.getElementById('bet-number').disabled = false;
-    document.getElementById('result').textContent = " ";
-}
+  document.getElementById("bet-number").disabled = false;
+  document.getElementById("result").textContent = " ";
+};
 
 const disable = () => {
-    document.getElementById('bet-number').disabled = false;
-    document.getElementById('result').textContent = 'Row/Col bets under development';
-}
+  document.getElementById("bet-number").disabled = false;
+  document.getElementById("result").textContent =
+    "Row/Col bets under development";
+};
 
 const enable = () => {
-    document.getElementById('bet-number').disabled = true;
-    document.getElementById('result').textContent = " ";
-}
+  document.getElementById("bet-number").disabled = true;
+  document.getElementById("result").textContent = " ";
+};
 
+numdis.addEventListener("click", og);
+
+evedis.addEventListener("click", enable);
+odddids.addEventListener("click", enable);
+reddis.addEventListener("click", enable);
+blackdis.addEventListener("click", enable);
+
+rowdis.addEventListener("click", disable);
+columndis.addEventListener("click", disable);
